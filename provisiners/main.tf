5db40ac330c47e394bbbe67ced7c1c7984c42a0f
91996 <mukunca50@gmail.com>
2023-04-15 19:35:30 +0530

    main.tf

---
+++ provisiners/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0089b8e98cd95257d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-04431cb92998376ef"]
+
+  tags = {
+    Name = "demo"
+  }
+
+  provisioner "remote-exec" {
+
+    connection {
+      host     = self.public_ip
+      user     = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "echo Hello"
+    ]
+  }
+
+}
+
